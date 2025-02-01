1d4d43ba494f1a44fb154986d89c9db0f645d1e9
IkshwakJ <157066906+IkshwakJ@users.noreply.github.com>
2025-02-01 15:35:31 -0500

    Time: 6 ms (6.39%) | Memory: 11.2 MB (19.88%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -5,6 +5,7 @@ public:
         int len_a = a.size() - 1, len_b = b.size() - 1, i = len_a, j = len_b;
         int transfer = 0;
         string c = "";
+        char x;
         while (i >= 0 || j >=0 || transfer == 1){
             if(i >= 0){
                 transfer += a[i] - '0'; // The ascii value of a[i] - the ascii value of '0'
@@ -17,7 +18,8 @@ public:
                 j--;          
             }
             // At this point transfer can have the values of 0, 1, or 2
-            c = to_string(transfer%2) + c;
+            x = transfer%2 + '0';
+            c =  x + c;
             transfer /= 2;
         }
         return c;
