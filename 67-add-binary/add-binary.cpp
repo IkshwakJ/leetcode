61d99636a8078f1997aeb59490386ea9f1f58844
IkshwakJ <157066906+IkshwakJ@users.noreply.github.com>
2025-02-01 15:31:30 -0500

    Time: 3 ms (26.81%) | Memory: 12.2 MB (11.00%) - LeetSync

---
+++ 67-add-binary/add-binary.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+
+        int len_a = a.size() - 1, len_b = b.size() - 1, i = len_a, j = len_b;
+        int transfer = 0;
+        string c = "";
+        while (i >= 0 || j >=0 || transfer == 1){
+            if(i >= 0){
+                transfer += a[i] - '0'; // The ascii value of a[i] - the ascii value of '0'
+                                        // 0 if a[i] is 0 otherwise 1 
+                i--;          
+            }
+            if(j >= 0){
+                transfer += b[j] - '0'; // The ascii value of a[i] - the ascii value of '0'
+                                        // 0 if a[i] is 0 otherwise 1 
+                j--;          
+            }
+            // At this point transfer can have the values of 0, 1, or 2
+            c = to_string(transfer%2) + c;
+            transfer /= 2;
+        }
+        return c;
+    }
+};
