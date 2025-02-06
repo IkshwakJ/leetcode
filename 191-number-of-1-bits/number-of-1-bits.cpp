4e6be4774079337ef1b5cf23b4314251310eed4e
IkshwakJ <157066906+IkshwakJ@users.noreply.github.com>
2025-02-06 16:53:27 -0500

    Time: 0 ms (100.00%) | Memory: 8.3 MB (47.54%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -4,7 +4,7 @@ public:
         int count = 0;
 
         while (n) {
-            n &= (n - 1);
+            n &= (n - 1); // This action removes the last/rightmost setbit. 
             count++;
         }
 
