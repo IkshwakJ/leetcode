df3ffe76e5ec14a00ca20a0dac9d3e7731ff522a
IkshwakJ <157066906+IkshwakJ@users.noreply.github.com>
2025-02-07 23:45:20 -0500

    Time: 75 ms (6.37%) | Memory: 207.3 MB (5.09%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
@@ -3,25 +3,23 @@ class Solution {
 public:
     int countPrimeSetBits(int left, int right) {
         vector<int> result (right+1,0);
-        int i = 1;
+        int i = left;
+        int temp, set_bit_cnt;
         int number_of_primes = 0;
         int primes[11] = {2,3,5,7,11,13,17,19,23,29,31};
-        while (i <= right) {
-            result[i] = result[i&(i-1)] + 1;
-            /*
-                i&(i-1) will give the last / nearest index to which there is only a difference of 1 bit.
-                We start at index 0 which has 0 set bit and index 1 is closest to index 0 seperated by one bit.
-                Example index 76 which is 1001100 in binary is just one bit seprated from index 72 which is 1001000.
-                76&75 will result in 72.
-            */
-            if(i >= left){
-                for(int j = 0; j < 11; j++){
-                    if(result[i] == primes[j]){
-                        number_of_primes ++;
-                    }
+        while (left <= right) {
+            temp = left;
+            set_bit_cnt = 0;
+            while(temp){
+                set_bit_cnt++;
+                temp &= (temp - 1);
+            }
+            for(int j = 0; j < 11; j++){
+                if(set_bit_cnt == primes[j]){
+                    number_of_primes ++;
                 }
             }
-            i++;
+            left++;
         }
         return number_of_primes;
     }
