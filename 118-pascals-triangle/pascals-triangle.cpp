c082dd90e7ec9da5aa041360f0df9a5785037cd5
IkshwakJ <157066906+IkshwakJ@users.noreply.github.com>
2025-02-08 22:49:06 -0500

    Time: 0 ms (100.00%) | Memory: 9.8 MB (32.96%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,28 +1,12 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> result;
-        if (numRows == 0){
-            return {};
-        }
-        if(numRows >= 1){
-            result.push_back({1});
-        }
-        if(numRows >= 2){
-            result.push_back({1,1});
-        }
-        
-        for(int i = 3; i <= numRows; i++){
-            vector<int> row;
-            for(int j = 0; j < i; j++){
-                if(j == 0 || j == i - 1){
-                    row.push_back(1);
-                }
-                else{
-                    row.push_back(result[i-2][j-1] + result[i-2][j]);
-                }
+        vector<vector<int>> result(numRows);
+        for(int i = 0; i < numRows; i++){
+            result[i].resize(i + 1, 1);
+            for (int j = 1; j < i; j++) {  // Avoid first and last elements
+                result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
             }
-            result.push_back(row);
         }
         return result;
     }
