593ccd8a427e47d3f58403b4b54335436873022b
IkshwakJ <157066906+IkshwakJ@users.noreply.github.com>
2025-02-09 20:14:34 -0500

    Time: 0 ms (100.00%) | Memory: 27.9 MB (99.92%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,14 +1,31 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        // Brute Force
-        int len = nums.size();
-        map<int,int> mp;
-        for(int num:nums){
-            if(++mp[num] > len/2){
-                return num;
+        /* 
+            Moore's Voting Algorithm
+            We consider count of votes to be 0.
+            (The first number will automatically get a vote)
+            When we encounter the same number as the majority we increment the count.
+            Otherwise we decrement it. 
+            When the count becomes 0, we place the next number as the majority and continue.
+            This works only becasue the majority is always more than half the lenght of the array.
+
+        */
+        int majority, count = 0;
+        for(int num : nums){
+            if (count == 0){
+                majority = num;
+                count++;
+            }
+            else{
+                if (num == majority){
+                    count++;
+                }
+                else{
+                    count--;
+                }
             }
         }
-        return -1;
+        return majority;
     }
 };
