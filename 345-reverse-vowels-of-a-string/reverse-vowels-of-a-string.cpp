7b5a766d4eed8b0f113f7b1aabef6771712d3375
IkshwakJ <157066906+IkshwakJ@users.noreply.github.com>
2025-02-11 23:40:22 -0500

    Time: 5 ms (19.06%) | Memory: 11.4 MB (8.77%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -6,10 +6,10 @@ public:
         int i  = 0;
         unordered_map<char,int> vowels {{'a',1},{'e',1},{'i',1},{'o',1},{'u',1},{'A',1},{'E',1},{'I',1},{'O',1},{'U',1}};
         while(i<j){
-            while(i<len && vowels[s[i]] != 1){
+            while(i<len && vowels[s[i]] != 1 && i<j){
                 i++;
             }
-            while(j>=0 && vowels[s[j]] != 1){
+            while(j>=0 && vowels[s[j]] != 1 && i<j){
                 j--;
             }
             if(i < j){
