041945d0d195882f2a0dad56adee0dc150653f9e
IkshwakJ <157066906+IkshwakJ@users.noreply.github.com>
2025-02-14 18:57:08 -0500

    Time: 3 ms (34.07%) | Memory: 9.9 MB (82.00%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -3,15 +3,15 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         int len = nums.size();
         int possible = 1<<len;
-        vector<vector<int>> result;
+        vector<vector<int>> result (possible);
         for(int i = 0; i < possible; i++){
             vector<int> row;
             for(int j = 0; j < len; j++){
-                if(i&1<<j){
+                if(i&(1<<j)){
                     row.push_back(nums[j]);
                 }
             }
-            result.push_back(row);
+            result[i] = row;
         }
         return result;
     }
