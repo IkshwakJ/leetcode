46c91510e657fac3b32b662632916e6f558430e3
IkshwakJ <157066906+IkshwakJ@users.noreply.github.com>
2025-02-15 14:14:54 -0500

    Time: 4 ms (21.83%) | Memory: 11.1 MB (30.94%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,50 +1,41 @@
 class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        set<vector<int>> result_set;
         int len = nums.size();
-        vector<vector<int>> result;
-        
-        for (int i = 0; i < (1 << len); i++) {
+        for(int i = 0; i < (1<<len); i++){
             vector<int> row;
-            for (int j = 0; j < len; j++) {
-                if (i & (1 << j)) {
+            for(int j = 0; j < len; j++){
+                if(i&(1<<j)){
                     row.push_back(nums[j]);
                 }
             }
-            
-            bool isDuplicate = false;
-            for (int k = 0; k < result.size(); k++) {
-                if (areEqual(result[k], row)) {
-                    isDuplicate = true;
-                    break;
-                }
+            if(row.size()){
+                sort_vector(row, 0, row.size() - 1);
             }
-            
-            if (!isDuplicate) {
-                result.push_back(row);
-            }
+            result_set.insert(row);
         }
-
+        vector<vector<int>> result(result_set.begin(), result_set.end());
         return result;
     }
-
 private:
-    bool areEqual(vector<int>& a, vector<int>& b) {
-        if (a.size() != b.size()) return false;
-
-        vector<int> visited(b.size(), 0); 
-
-        for (int i = 0; i < a.size(); i++) {
-            bool found = false;
-            for (int j = 0; j < b.size(); j++) {
-                if (a[i] == b[j] && !visited[j]) {
-                    visited[j] = 1; 
-                    found = true;
-                    break;
-                }
-            }
-            if (!found) return false;
+    void sort_vector(vector<int>& nums, int low, int high){
+        if(low < high){
+            int piv = partition(nums,low,high);
+            sort_vector(nums, low, piv - 1);
+            sort_vector(nums, piv + 1, high);
         }
-        return true;
+    }
+    int partition(vector<int>& nums, int low, int high){
+        int pivot = nums[high];
+        int i = low;
+        for(int j = low; j < high; j++){
+            if(nums[j] <= pivot){
+                swap(nums[j], nums[i]);
+                i++;
+            }
+        }
+        swap(nums[i], nums[high]);
+        return i;
     }
 };
