37aa7855dac28b64dc2248485015302bfd44abef
IkshwakJ <157066906+IkshwakJ@users.noreply.github.com>
2025-02-25 20:04:10 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (18.16%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -5,33 +5,46 @@ public:
         if (len == 0){
             return {-1,-1};
         }
-        vector<int> pos{-1,-1};
-        int left = 0; 
-        int right = len - 1;
-        while(left < right){
+        int left = findFirst(nums, target, len);
+        if(left == -1){
+            return {-1,-1};
+        }
+        return {left, findLast(nums, target, len)};
+
+    }
+private: 
+    int findFirst(vector<int>& nums, int target, int len){
+        int left = 0;
+        int right = len - 1; 
+        while(left <= right){
             int mid = left + (right - left)/2;
-            if(nums[mid] == target){
-                left = mid; 
-                right = mid;
-            }
-            else if(nums[mid] > target){
+            if(nums[mid] >= target){
                 right = mid - 1;
             }
             else{
                 left = mid + 1;
             }
         }
-        if(left != right || nums[left] != target){
-            return {-1,-1};
+        if(left < len && nums[left] == target){
+            return left;
         }
-        while(right < len && nums[right] == target){
-            right++;
+        return -1;
+    }
+    int findLast(vector<int>& nums, int target, int len){
+        int left = 0;
+        int right = len - 1; 
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(nums[mid] <= target){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
         }
-        right--;
-        while(left >= 0 && nums[left] == target){
-            left--;
+        if(right >= 0 && nums[right] == target){
+            return right;
         }
-        left++;
-        return {left,right};
+        return -1;
     }
 };
